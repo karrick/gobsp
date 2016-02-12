42d6f5bf9518c9be54bd916d2f3794383799fd43
Karrick S. McDermott <kmcdermott@linkedin.com>
2016-02-11 19:40:31 -0500

    scanner working

--- scanner.go
+++ scanner.go
@@ -6,32 +6,94 @@ import (
 	"io/ioutil"
 )
 
+// MessageType is a variable width integer that specifies which user-defined type a particular
+// message payload should be interpreted as.
 type MessageType VWI
 
+// ErrScannerHasNoHandlers is an error that is returned when NewScanner is called without any
+// message handlers.
+type ErrScannerHasNoHandlers struct{}
+
+func (e ErrScannerHasNoHandlers) Error() string {
+	return "scanner has no message handlers"
+}
+
+// ErrUnknownMessageType is an error that is returned by Scanner.Handle() when the required
+// user-defined message type has not been defined and there is no default message handler.
 type ErrUnknownMessageType VWI
 
 func (e ErrUnknownMessageType) Error() string {
 	return "unknown message type: " + VWI(e).String()
 }
 
+// MessageHandler is any function that consumes the entirety of the specified io.Reader stream and
+// returns any error that occurred while processing that message.
 type MessageHandler func(io.Reader) error
 
-func NewScanner(ior io.Reader, handlers map[uint32]MessageHandler) *Scanner {
-	s := &Scanner{
-		bufferedReader: bufio.NewReader(ior),
-		handlers:       handlers,
+// ScannerConfigurator is a function that modifies a newly created Scanner instance.
+type ScannerConfigurator func(*Scanner) error
+
+// DefaultHandler specifies a handler to invoke when the required message type does not have a
+// defined handler.
+func DefaultHandler(handler MessageHandler) ScannerConfigurator {
+	return func(s *Scanner) error {
+		s.defaultHandler = handler
+		return nil
 	}
-	return s
 }
 
+// Handlers is used to specify the user-defined message types for a Scanner instance.
+func Handlers(handlers map[uint32]MessageHandler) ScannerConfigurator {
+	return func(s *Scanner) error {
+		s.handlers = handlers
+		return nil
+	}
+}
+
+// NewScanner returns a new Scanner instance to process messages from the specified io.Reader
+// stream, using the message handlers specified by the DefaultHandler and Handlers functions.
+func NewScanner(ior io.Reader, configurators ...ScannerConfigurator) (*Scanner, error) {
+	s := &Scanner{
+		bufferedReader: bufio.NewReader(ior),
+	}
+	for _, c := range configurators {
+		if err := c(s); err != nil {
+			return nil, err
+		}
+	}
+	if s.defaultHandler == nil && s.handlers == nil {
+		return nil, ErrScannerHasNoHandlers{}
+	}
+	return s, nil
+}
+
+// Scanner defines an object used to scan binary messages from a stream of bytes from a particular
+// io.Reader.
 type Scanner struct {
 	bufferedReader           *bufio.Reader
 	err                      error
 	messageType, messageSize VWI
 	handlers                 map[uint32]MessageHandler
+	defaultHandler           MessageHandler
 }
 
+// Err returns the error object associated with this scanner, or nil
+// if no errors have occurred.
+func (s *Scanner) Err() error {
+	return s.err
+}
+
+// Reset sets the scannner's error state to nil.
+func (s *Scanner) Reset() {
+	s.err = nil
+}
+
+// Scan reads enough bytes from the stream to determine the message type and size. Normally returns
+// true, but returns false on error or EOF.
 func (s *Scanner) Scan() bool {
+	if s.err != nil {
+		return false
+	}
 	if s.err = s.messageType.UnmarshalBinaryFrom(s.bufferedReader); s.err != nil {
 		if s.err == io.EOF {
 			s.err = nil
@@ -47,23 +109,30 @@ func (s *Scanner) Scan() bool {
 	return true
 }
 
-// Err returns the error object associated with this scanner, or nil
-// if no errors have occurred.
-func (s *Scanner) Err() error {
-	return s.err
-}
-
+// Handle invokes the message handler for the most recently received message type. If the required
+// message handler is not defined, it invokes the default handler. If there is no default handler,
+// it returns an error.
 func (s *Scanner) Handle() error {
 	if s.err != nil {
 		return s.err
 	}
-
 	limitReader := io.LimitReader(s.bufferedReader, int64(s.messageSize))
 	handler, ok := s.handlers[uint32(s.messageType)]
 	if !ok {
-		io.Copy(ioutil.Discard, limitReader) // consume unknown message
-		s.err = ErrUnknownMessageType(s.messageType)
-		return s.err
+		if s.defaultHandler == nil {
+			DiscardAll(limitReader)
+			s.err = ErrUnknownMessageType(s.messageType)
+			return s.err
+		}
+		s.defaultHandler(limitReader)
+		return nil
 	}
 	return handler(limitReader)
+}
+
+// DiscardAll discards the remaining bytes to be read from the specified io.Reader, returning any
+// errors received while reading.
+func DiscardAll(ior io.Reader) error {
+	_, err := io.Copy(ioutil.Discard, ior)
+	return err
 }
--- scanner_test.go
+++ scanner_test.go
@@ -22,16 +22,32 @@ func ensure(t *testing.T, actual, expected interface{}) {
 	}
 }
 
+func TestBinaryScannerNoHandlers(t *testing.T) {
+	bb := new(bytes.Buffer)
+	_, err := NewScanner(bb)
+	if _, ok := err.(ErrScannerHasNoHandlers); err == nil || !ok {
+		t.Errorf("Actual: %#v; Expected: %#v", err, ErrScannerHasNoHandlers{})
+	}
+}
+
 func TestBinaryScannerEOF(t *testing.T) {
 	bb := new(bytes.Buffer)
 	handlers := make(map[uint32]MessageHandler)
-	scanner := NewScanner(bb, handlers)
+
+	scanner, err := NewScanner(bb, Handlers(handlers))
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	ensure(t, scanner.Scan(), false)
 	ensure(t, scanner.Err(), nil)
 }
 
 func TestBinaryScannerHandleReturnsHandlerError(t *testing.T) {
+	bb := bytes.NewBuffer([]byte{
+		0x00, 0x00, 0x01, 0x00,
+	})
+
 	handlers := map[uint32]MessageHandler{
 		0x00: func(io.Reader) error {
 			return io.EOF
@@ -41,44 +57,78 @@ func TestBinaryScannerHandleReturnsHandlerError(t *testing.T) {
 		},
 	}
 
-	bb := bytes.NewBuffer([]byte{
-		0x00, 0x00, 0x01, 0x00,
-	})
-
-	scanner := NewScanner(bb, handlers)
+	scanner, err := NewScanner(bb, Handlers(handlers))
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	ensure(t, scanner.Scan(), true)
 	ensure(t, scanner.Handle(), io.EOF)
+
 	ensure(t, scanner.Scan(), true)
 	ensure(t, scanner.Handle(), nil)
+
 	ensure(t, scanner.Scan(), false)
 	ensure(t, scanner.Err(), nil)
 }
 
-func TestBinaryScannerHandleErrUnknownMessageTypeSkipsBadMessage(t *testing.T) {
-	handlers := map[uint32]MessageHandler{
-		0x00: func(ior io.Reader) error {
-			return nil
-		},
-	}
-
+func TestBinaryScannerHandleUnknownMessageTypeWithoutDefaultHandler(t *testing.T) {
 	bb := bytes.NewBuffer([]byte{
 		0x00, 0x00, // normal message
 		0x01, 0x04, 0xDE, 0xAD, 0xBE, 0xEF, // unknown message and payload
 		0x00, 0x00, // normal message to ensure it keeps going
 	})
 
-	scanner := NewScanner(bb, handlers)
+	handlers := map[uint32]MessageHandler{
+		0x00: func(ior io.Reader) error {
+			return nil
+		},
+	}
+
+	scanner, err := NewScanner(bb, Handlers(handlers))
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	ensure(t, scanner.Scan(), true)
 	ensure(t, scanner.Handle(), nil)
-	ensure(t, scanner.Scan(), true)
 
-	// expect ErrUnknownMessageType, and consume unknown message
+	ensure(t, scanner.Scan(), true)
 	ensure(t, scanner.Handle(), ErrUnknownMessageType(0x01))
 
+	ensure(t, scanner.Scan(), false) // ??? b/c it remembers error ???
+	ensure(t, scanner.Err(), ErrUnknownMessageType(0x01))
+}
+
+func TestBinaryScannerHandleUnknownMessageTypeWithDefaultHandler(t *testing.T) {
+	bb := bytes.NewBuffer([]byte{
+		0x00, 0x00, // normal message
+		0x01, 0x04, 0xDE, 0xAD, 0xBE, 0xEF, // unknown message and payload
+		0x00, 0x00, // normal message to ensure it keeps going
+	})
+
+	handlers := map[uint32]MessageHandler{
+		0x00: func(ior io.Reader) error {
+			return nil
+		},
+	}
+
+	scanner, err := NewScanner(bb,
+		DefaultHandler(DiscardAll),
+		Handlers(handlers))
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	ensure(t, scanner.Scan(), true)
 	ensure(t, scanner.Handle(), nil)
+
+	ensure(t, scanner.Scan(), true)
+	ensure(t, scanner.Handle(), nil)
+
+	ensure(t, scanner.Scan(), true)
+	ensure(t, scanner.Handle(), nil)
+
 	ensure(t, scanner.Scan(), false)
 	ensure(t, scanner.Err(), nil)
 }
